1a07c776a09f19d66bbdb55eb38d9692ea37a35b
Leopoldo Orines <leoorines@gmail.com>
2023-04-11 16:43:31 -0400

    add binanace smart chain driver

---
+++ drivers/binance/client.go
@@ -0,0 +1,81 @@
+package binance
+
+import (
+	"context"
+	"fmt"
+	"github.com/datadaodevs/evm-etl/client/node"
+	protos "github.com/datadaodevs/evm-etl/protos/go/protos/chains/binance"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+type client struct {
+	innerClient node.Client
+}
+
+// GetLatestBlockNumber gets the most recent block number
+func (c *client) GetLatestBlockNumber(ctx context.Context) (uint64, error) {
+	number, err := c.innerClient.GetLatestBlockNumber(ctx)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+
+// GetBlockByNumber gets a block by number
+func (c *client) GetBlockByNumber(ctx context.Context, blockNumber uint64) (*protos.Block, error) {
+	res, err := c.innerClient.GetBlockByNumber(ctx, blockNumber)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &protos.Block{}
+	if err := protojson.Unmarshal(res.Result, data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (c *client) GetTracesForBlock(ctx context.Context, blockNumber uint64) ([]*protos.CallTrace, error) {
+	// genesis block has no traces
+	if blockNumber == 0 {
+		return nil, nil
+	}
+
+	res, err := c.innerClient.GetTracesForBlock(ctx, blockNumber)
+	if err != nil {
+		return nil, err
+	}
+
+	var rawTraces []*protos.CallTrace
+	for _, trace := range res.Result {
+		if trace.Error != nil {
+			return nil, fmt.Errorf("%v", trace.Error)
+		}
+		rawTrace := &protos.CallTrace{}
+		if err := protojson.Unmarshal(trace.Result, rawTrace); err != nil {
+			return nil, err
+		}
+		rawTraces = append(rawTraces, rawTrace)
+	}
+
+	return rawTraces, nil
+}
+
+func (c *client) GetBlockReceipt(ctx context.Context, blockNumber uint64) ([]*protos.TransactionReceipt, error) {
+	res, err := c.innerClient.GetBlockReceipt(ctx, blockNumber)
+	if err != nil {
+		return nil, err
+	}
+
+	var rawReceipts []*protos.TransactionReceipt
+	for _, receipt := range res.Result {
+		rawReceipt := &protos.TransactionReceipt{}
+		if err := protojson.Unmarshal(receipt, rawReceipt); err != nil {
+			return nil, err
+		}
+		rawReceipts = append(rawReceipts, rawReceipt)
+	}
+
+	return rawReceipts, nil
+}
---
+++ drivers/binance/validate.go
@@ -0,0 +1,27 @@
+package binance
+
+import (
+	"context"
+	"errors"
+)
+
+// IsValidBlock checks the given block's parent hash against the hash of the previous block
+func (d *Driver) IsValidBlock(ctx context.Context, index uint64) error {
+	d.logger.Infof("comparing block %d to block %d for validation", index, index-1)
+
+	currentBlock, err := d.getBlockByNumber(ctx, index)
+	if err != nil {
+		return err
+	}
+	previousBlock, err := d.store.RetrieveBlock(ctx, index-1)
+	if err != nil {
+		return err
+	}
+
+	if currentBlock.ParentHash != previousBlock.Hash {
+		d.logger.Infof("chain reorg detected at block %d", previousBlock.Number)
+		return errors.New("new block parent hash does not match previous block hash")
+	}
+
+	return nil
+}
